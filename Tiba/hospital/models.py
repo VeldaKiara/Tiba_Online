5f3a230996d8157fecf54c660731be2f2aab70ef
veldakarimi <veldakarimi@gmail.com>
2020-12-30 01:34:27 +0300

    addition of user_hospital to hospital model

--- Tiba/hospital/models.py
+++ Tiba/hospital/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import uuid
+from accounts.models import *
 # Create your models here.
+
 HOSPITAL_LEVELS=[
     ('1','Community Facilities, Level 1'),
     ('2','Health Dispensaries, Level 2'),
@@ -20,6 +22,17 @@ class Hospital(models.Model):
     sub_county = models.CharField(max_length=250)
     hospital_level = models.CharField(choices=HOSPITAL_LEVELS, default=1,  max_length=250)
     is_referral = models.BooleanField(default=False, choices=BOOL_CHOICES, max_length=25)
+    class Meta:
+        pass
+
+class User_Hospital(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    hospital_id = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+    class Meta:
+        pass
+
+    
     
     
     
