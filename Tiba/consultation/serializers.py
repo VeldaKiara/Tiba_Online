38c942bc9a403261b7032f4d7599eea39c0d97f2
veldakarimi <veldakarimi@gmail.com>
2021-01-07 19:32:26 +0300

    added serializers in consultation app

--- Tiba/consultation/serializers.py
+++ Tiba/consultation/serializers.py
@@ -4,4 +4,14 @@ from consultation.models import Consultation, Reffer, Admit
 class ConsultationSerializer(serializers.ModelSerializer):
     	class Meta:
 		model = Consultation
-		fields=('id','condition','user_id')
+		fields=('id','','symptoms','temparature','blood_pressure_a','blood_pressure_b','diagnosis','doctor_id','user_id')
+  
+class RefferSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reffer
+        fields = ('id', 'hospital','note_to_specialist','current_prescriptions')
+        
+class AdmitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Admit
+        fields = ('id', 'ward', 'specialist', 'note_to_specialist', 'current_prescriptions')
